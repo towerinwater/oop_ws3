9988eca02f8409602208fa8acae78c66fa72b0dd
Tri Hai <haitri62@gmail.com>
2024-03-13 18:56:31 +1030

    Finished Work

--- main.cpp
+++ main.cpp
@@ -2,6 +2,16 @@
 #include "workshop.h"
 
 int main(){
+    double arr[10] = {1,2,3,4,5,6,7,8,9,10};
+    int max = arrayMax(arr, 10);
 
+    double num = 99.99;
+    changeValue(&num);
+
+    printArray(arr, 10);
+
+    double* pArr = dynamicArray(10, 99.99);
+
+    
     return 0;
 }
