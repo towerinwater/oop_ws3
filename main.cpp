95d081e7fbdb93d502d406d87f3903e34f35ff00
Tri Hai <haitri62@gmail.com>
2024-03-13 19:00:23 +1030

    Finished Work

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 
 int main(){
     double arr[10] = {1,2,3,4,5,6,7,8,9,10};
-    int max = arrayMax(arr, 10);
+    double max = arrayMax(arr, 10);
+    std::cout << max <<std::endl;
 
     double num = 99.99;
     changeValue(&num);
@@ -11,7 +12,10 @@ int main(){
     printArray(arr, 10);
 
     double* pArr = dynamicArray(10, 99.99);
+    for(int i = 0; i < 10; i++){
+        std::cout << pArr[i];
+    }
+    std::cout << std::endl;
 
-    
     return 0;
 }
